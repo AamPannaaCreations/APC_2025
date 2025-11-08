6b33099ed7bf46a09b2db914d9df1bacadfb56a1
AamPannaaCreations <166213685+AamPannaaCreations@users.noreply.github.com>
2025-11-08 21:09:21 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CG5WRLK');</script>
+<!-- End Google Tag Manager -->
+
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -116,6 +126,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${bricolageGrotesque.variable} antialiased`}
       >
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CG5WRLK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <Navbar />
         <Toaster />
         {children}
