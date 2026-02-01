d21e71716bbdd4a01aabfc7a2b2f56bf46b1f3ae
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2026-02-01 17:25:57 +0530

    Update Journey.tsx

--- components/anniversary/Journey.tsx
+++ components/anniversary/Journey.tsx
@@ -4,7 +4,7 @@ import { RetroGrid } from "@/components/ui/retro-grid"
 
 export default function Journey() {
 return (
-  <div className="bg-[#FFF1C3] py-5" style={{ height: "600px", position: "relative" }}>
+  <div className="bg-[#FFF1C3] " style={{ height: "600px", position: "relative" }}>
     <h1 className="text-center font-semibold text-6xl text-[#8C6400]">Our Journey in moments</h1>
     <div className="relative h-[900px] w-full overflow-hidden">
       <RetroGrid />
